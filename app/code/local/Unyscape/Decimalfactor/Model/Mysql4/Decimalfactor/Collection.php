211b6c21a4bf6a203310b6773892a651bc165bb4
prernaunyscape <42672091+prernaunyscape@users.noreply.github.com>
2018-08-24 17:24:39 +0530

    Collection.php

---
+++ app/code/local/Unyscape/Decimalfactor/Model/Mysql4/Decimalfactor/Collection.php
@@ -0,0 +1,12 @@
+<?php
+    class Unyscape_Decimalfactor_Model_Mysql4_Decimalfactor_Collection extends Mage_Core_Model_Mysql4_Collection_Abstract
+    {
+
+		public function _construct(){
+			$this->_init("decimalfactor/decimalfactor");
+		}
+
+		
+
+    }
+	 
