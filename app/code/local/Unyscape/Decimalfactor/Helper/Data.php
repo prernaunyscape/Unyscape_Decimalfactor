934980da64357135310c02eef66845b3d980a8a8
prernaunyscape <42672091+prernaunyscape@users.noreply.github.com>
2018-08-24 17:23:45 +0530

    Data.php

---
+++ app/code/local/Unyscape/Decimalfactor/Helper/Data.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Custom Module Sum Multiplied By The Decimal Factor 
+ *
+ * @author     Prerna
+ */
+class Unyscape_Decimalfactor_Helper_Data extends Mage_Core_Helper_Abstract
+{
+      
+}
